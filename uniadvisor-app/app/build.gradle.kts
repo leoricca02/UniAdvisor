ba9e23ef50fca22a2d14254804269513643a438f
leoricca02 <leo.ricca@stud.uniroma3.it>
2025-10-20 23:48:13 +0200

    login, signup, faculty_choosing, courses-list, course_page

--- uniadvisor-app/app/build.gradle.kts
+++ uniadvisor-app/app/build.gradle.kts
@@ -58,7 +58,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.activity.compose)
 
     // Test dependencies
     testImplementation(libs.junit)
